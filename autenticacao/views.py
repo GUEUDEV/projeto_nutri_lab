55956e31893fe829581e50b96fd66ed84e505bd8
gustavo <gustavoptlsoliveira@gmail.com>
2025-02-06 10:27:37 -0300

    pouco

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
 
 def logar(request):
     return HttpResponse('login efetuado com sucesso!!!')
